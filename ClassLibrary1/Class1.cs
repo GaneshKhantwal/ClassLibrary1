3cce5cfab705a8ac7719af853abf302d76d0eabe
GaneshKhantwal <ganesh.khantwal@niit-tech.com>
2015-07-09 20:02:35 +0530

    ok

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public void MethodA(Class2 cls2)
+        {
+            //Class2 cls2 = new Class2();
+            cls2.MethodB(this);
+
+        }
+    }
+}
---
+++ ClassLibrary2/Class2.cs
@@ -0,0 +1,18 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2
+{
+    public class Class2
+    {
+        public void MethodB(Class1 cls1)
+        {
+            cls1.MethodA(this);
+         
+        }
+    }
+}
